fd289d6dff161f33d1208c585e820b723827c9c9
Duy Quang <qvuduyy0501@gmail.com>
2026-03-09 18:15:57 +0700

    Refactor SongPlayer structure and organize event handling

--- script.js
+++ script.js
@@ -21,9 +21,71 @@ class SongPlayer
     this.audio  = audioElement;
     this.songs = songList;
     this.currentIndex = 0;
+    //this.player = document.getElementById("player");
+    this.playButton = document.getElementById("playBtn");
+    this.backButton = document.getElementById("backBtn");
+    this.forwardButton = document.getElementById("forwardBtn");
+    this.progressBar = document.getElementById("progressBar");
+    this.totalTime = document.getElementById("totalTime");
+    this.currentTime = document.getElementById("currentTime");
+    this.songName = document.getElementById("songName");
   }
 
-  // Methods:
+  // -- -- -- Methods: -- -- --
+  // -- -- Initialize button functions -- --
+  bindEvents()
+  {
+    //-- Pause/Play --
+    this.playButton.addEventListener("click", () => {
+    
+      if(this.audio.paused)
+      {
+        if(!this.audio.src)
+        {
+          this.loadSong();
+        }
+        this.play();
+        this.playButton.innerHTML = '<i class = "fa-solid fa-pause"></i>';
+      }
+      else
+      {
+        this.pause();
+        this.playButton.innerHTML = '<i class = "fa-solid fa-play"></i>';
+      }
+  })
+    // -- Next Song -- 
+    this.forwardButton.addEventListener("click", () => {
+    this.next();
+    this.playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+  })
+    // -- Previous Song -- 
+  this.backButton.addEventListener("click", () => {
+    this.back();
+    this.playButton.innerHTML = '<i class="fa-solid fa-pause"></i>'; 
+  })
+  this.audio.addEventListener("loadedmetadata", () => {
+    this.progressBar.max = this.audio.duration;
+    this.songName.textContent = this.currentSongName();
+    // Update max time
+    this.totalTime.textContent = formatTime(this.audio.duration);
+  });
+
+  this.audio.addEventListener("timeupdate", () => {
+    this.progressBar.value = this.audio.currentTime;
+    // Updated time logic
+    this.currentTime.textContent = formatTime(this.audio.currentTime);
+  });
+
+  this.progressBar.addEventListener("input", () => {
+    this.audio.currentTime = this.progressBar.value;
+  });
+
+  this.audio.addEventListener("ended", () => {
+    this.next();
+    this.playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+  })
+
+  }
   // load the song
   loadSong()
   {
@@ -34,6 +96,7 @@ class SongPlayer
   {
     this.audio.play();
   }
+  // pause the song
   pause()
   {
     this.audio.pause();
@@ -44,7 +107,8 @@ class SongPlayer
     this.currentIndex = (this.currentIndex + 1) % this.songs.length;
     this.loadSong();
     this.play();
-  }// previous song
+  }
+  // previous song
   back()
   {
     if(this.currentIndex == 0) this.currentIndex = this.songs.length - 1;
@@ -52,10 +116,13 @@ class SongPlayer
     this.loadSong();
     this.play();
   }
+  // return the song name
   currentSongName()
   {
     return this.songs[this.currentIndex].name;
   }
+
+
 }
 
 function formatTime(seconds)
@@ -69,69 +136,14 @@ function formatTime(seconds)
   }
   return minute + ":" + second;
 }
-const player = document.getElementById("player");
-const playButton = document.getElementById("playBtn");
-const backButton = document.getElementById("backBtn");
-const forwardButton = document.getElementById("forwardBtn");
-const progressBar = document.getElementById("progressBar");
-const totalTime = document.getElementById("totalTime");
-const currentTime = document.getElementById("currentTime");
-const songName = document.getElementById("songName");
 
 const songs = [
   new Song("No Role Modelz", "Songs/NoRoleModelz.mp3"),
   new Song("She Knows", "Songs/SheKnows.mp3"),
   new Song("Wet Dreamz", "Songs/WetDreamz.mp3"),
-  new Song("Wind", "Songs/Wind.mp3")
+  new Song("Wind", "Songs/Wind.mp3"),
+  new Song("Luv Sic", "Songs/luv_sic.mp3")
 ];
 
-const songPlayer = new SongPlayer(player, songs);
-
-playButton.addEventListener("click", function() {
-  if (player.paused)
-  {
-    if(!player.src)
-    {
-      songPlayer.loadSong();
-    }
-    songPlayer.play();
-    playButton.innerHTML = '<i class = "fa-solid fa-pause"></i>';
-  }
-  else
-  {
-    songPlayer.pause();
-    playButton.innerHTML = '<i class = "fa-solid fa-play"></i>';
-  }
-})
-
-forwardButton.addEventListener("click", function() {
-  songPlayer.next();
-  playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
-})
-
-backButton.addEventListener("click", function() {
-  songPlayer.back();
-  playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
-})
-player.addEventListener("loadedmetadata", function() {
-  progressBar.max = player.duration;
-  songName.textContent = songPlayer.currentSongName();
-  // Update max time
-  totalTime.textContent = formatTime(player.duration);
-});
-
-player.addEventListener("timeupdate", function() {
-  progressBar.value = player.currentTime;
-  // Updated time logic
-  currentTime.textContent = formatTime(player.currentTime);
-});
-
-progressBar.addEventListener("input", function() {
-  player.currentTime = progressBar.value;
-});
-
-player.addEventListener("ended", function() {
-  songPlayer.next();
-  playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
-})
-
+const songPlayer = new SongPlayer(document.getElementById("player"),songs);
+songPlayer.bindEvents();
